058d715ec4cfa14f3c5774d64f5abd9011a8d3e9
Saul M <saulmm2@gmail.com>
2015-07-20 05:40:39 +0200

    Added resources and logic

--- presentation/src/main/java/saulmm/gradlestuff/MainActivity.java
+++ presentation/src/main/java/saulmm/gradlestuff/MainActivity.java
@@ -1,34 +1,21 @@
 package saulmm.gradlestuff;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
 	@Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
 
-	@Override public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
+		TextView ad 	= (TextView) findViewById(R.id.activity_main_ad);
+		TextView flavor = (TextView) findViewById(R.id.activity_main_flavor);
+		flavor.setText("Flavor: "+BuildConfig.FLAVOR);
 
-	@Override public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
+		if (BuildConfig.ADS)
+			ad.setVisibility(View.VISIBLE);
 	}
 }
